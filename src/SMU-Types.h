a4cb347f43a7758c1707526e1d9b9ef1aa4f69cc
Jonas Merkle [JJM] <jonas@jjm.one>
2020-02-01 21:57:32 +0100

    Types added

--- src/SMU-Types.h
+++ src/SMU-Types.h
@@ -40,9 +40,10 @@ namespace smu_types {
     enum SensorType : uint8_t {
         NONE, 
         BNO055_T, 
+        SRF08_T
+        //CMPS10,
         //VL53L0X_T, 
         //VL6180X_T, 
-        SRF08_T
     };
 
     /**
@@ -56,9 +57,42 @@ namespace smu_types {
          * @enum StatusCode
          * 
          * @brief Enum of all supportet sensor model names.
+         * 
+         * Codes <  1000 -> Major status code.
+         * Codes >= 1000 -> Fine software information.
          */
-        enum Status : int16_t {
-            DEFAULT = 0, 
+        enum Status : uint16_t {
+
+            // major status codes
+
+            STATUS_NONE = 0,
+
+            STATUS_ReadMsg = 1,
+            STATUS_ProcessMsg = 2,
+            STATUS_UpdateSensorReadings = 3,
+
+            STATUS_Sensors_Init_BNO055 = 4, 
+            STATUS_Sensors_Init_VL53L0X = 5, 
+            STATUS_Sensors_Init_VL6180X = 6,
+            STATUS_Sensors_Init_SRF08 = 7,
+            STATUS_Sensors_Init_CMPS10 = 8,
+
+
+            // fine software informations
+
+            INFO_MainSetup = 1000,
+            INFO_MainLoop = 1001,
+            INFO_ComNo4BytesRec = 1002,
+            INFO_ProcessMsgDone = 1003,
+            INFO_AutoUpdateDisables = 1004,
+
+            INFO_Sensors_activateSensor = 1009,
+		    INFO_Sensors_deactivateSensor = 1010,
+		    INFO_Sensors_isActive = 1011,
+		    INFO_Sensors_disconnectSensor = 1012,
+		    INFO_Sensors_updateSensor = 1013,
+		    INFO_Sensors_updateAllSensors = 1014,
+		    INFO_Sensors_getReading = 1015,
         };
 
         /**
@@ -70,49 +104,51 @@ namespace smu_types {
          * Codes > 0 -> Warning
          */
         enum Error : int16_t {
-            NONE = 0, 
+            ERR_NONE = 0, 
 
             // warnings
 
-            SensorInit = 200,
-            SensorInitBNO055 = 201,
-            SensorInitVL53L0X = 202,
-            SensorInitVL6180X = 203,
-            SensorInitSRF08 = 204,
+            WARN_SensorInit = 200,
+            WARN_SensorInitBNO055 = 201,
+            WARN_SensorInitVL53L0X = 202,
+            WARN_SensorInitVL6180X = 203,
+            WARN_SensorInitSRF08 = 204,
 
-            SensorReading = 250,
-            SensorReadingBNO055 = 251,
-            SensorReadingVL53L0X = 252,
-            SensorReadingVL6180X = 253,
-            SensorReadingSRF08 = 254,
+            WARN_SensorReading = 250,
+            WARN_SensorReadingBNO055 = 251,
+            WARN_SensorReadingVL53L0X = 252,
+            WARN_SensorReadingVL6180X = 253,
+            WARN_SensorReadingSRF08 = 254,
+
+            WARN_ComCouldNotReadIncommingMsg = 1000,
 
 
             // errors
 
-            Fatal = -1,
+            ERROR_Fatal = -1,
 
-            OmeError = -100,
-            OmeInvalidObjSize = -101,
-            OmeNoMemorSliceInWishedSizeAvaliable = -102,
-            OmeMaxNumberOfObjInPoolReached = -103,
-            OmePoolObjeOutOfRange = -104,
-            OmeObjPtrOutOfRange = -105,
-            OmeObjPtrIsNotPointingToTheFirstElementOfTheMemorySlice = -106,
+            ERR_OmeError = -100,
+            ERR_OmeInvalidObjSize = -101,
+            ERR_OmeNoMemorSliceInWishedSizeAvaliable = -102,
+            ERR_OmeMaxNumberOfObjInPoolReached = -103,
+            ERR_OmePoolObjeOutOfRange = -104,
+            ERR_OmeObjPtrOutOfRange = -105,
+            ERR_OmeObjPtrIsNotPointingToTheFirstElementOfTheMemorySlice = -106,
 
-            SensorInit = -200,
-            SensorInitBNO055 = -201,
-            SensorInitVL53L0X = -202,
-            SensorInitVL6180X = -203,
-            SensorInitSRF08 = -204,
+            ERR_SensorInit = -200,
+            ERR_SensorInitBNO055 = -201,
+            ERR_SensorInitVL53L0X = -202,
+            ERR_SensorInitVL6180X = -203,
+            ERR_SensorInitSRF08 = -204,
 
-            SensorReading = -250,
-            SensorReadingBNO055 = -251,
-            SensorReadingVL53L0X = -252,
-            SensorReadingVL6180X = -253,
-            SensorReadingSRF08 = -254
+            ERR_SensorReading = -250,
+            ERR_SensorReadingBNO055 = -251,
+            ERR_SensorReadingVL53L0X = -252,
+            ERR_SensorReadingVL6180X = -253,
+            ERR_SensorReadingSRF08 = -254,
+
+            ERR_ComCouldNotProcessCurrentMsg = -1000
         };
-
-    
     }
 }
 
